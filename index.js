8313432e100fec92f096de8c2f132ea4178e188a
Yeqing Liu <sunsetpeak42@gmail.com>
2018-07-13 14:33:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 let recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value){
-  let newObj = Object.assign({},object, {key: value});
+  let newObj = Object.assign({},object, {[key]: value});
   return newObj;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  return Object.assign(object, {key: value});
+  return Object.assign(object, {[key]: value});
 }
 
 function deleteFromObjectByKey(object, key){
