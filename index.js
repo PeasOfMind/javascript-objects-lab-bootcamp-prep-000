c2e344953b450941052c741e6c3053c0e8a96f6e
Yeqing Liu <sunsetpeak42@gmail.com>
2018-07-13 14:29:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,6 @@ function updateObjectWithKeyAndValue(object, key, value){
   return newObj;
 }
 
-function updateObjectWith
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+}
